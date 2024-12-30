2e18ce10ad48011d568af24c2b6a1a40ebc2e1f2
Stiaan Bekker <stiaanb@infoslips.co.za>
2024-12-30 17:09:51 +0200

    Init

---
+++ NitroTextFieldParser/NitroTextFieldParserTests/Helpers/TestDataHelper.cs
@@ -0,0 +1,54 @@
+﻿using NitroTextFieldParserTests.BenchMarks.Models;
+
+namespace NitroTextFieldParserTests.BenchMarks.Helpers;
+
+public static class TestDataHelper
+{
+  public static MemoryStream GetSampleDataAsStream(int count)
+  {
+    var data = GetSampleData(count);
+    var stream = new MemoryStream();
+    using var writer = new StreamWriter(stream, leaveOpen:true);
+    writer.WriteLine(SimpleCsvRowPopulated.GetCsvHeader());
+    foreach (var row in data)
+    {
+      writer.WriteLine(row.ToString());
+    }
+    writer.Flush();
+    stream.Position = 0;
+    return stream;
+  }
+
+  public static List<SimpleCsvRowPopulated> GetSampleData(int count)
+  {
+    var faker = new Bogus.Faker();
+    var result = new List<SimpleCsvRowPopulated>(count);
+    for (int i = 0; i < count; i++)
+    {
+      var dateOfBirth = faker.Date.Past();
+      result.Add(new SimpleCsvRowPopulated()
+      {
+        FirstName = faker.Name.FirstName(),
+        LastName = faker.Name.LastName(),
+        DateOfBirth = dateOfBirth,
+        Age = GetAge(dateOfBirth),
+        AddressLine1 = faker.Address.StreetAddress(),
+        AddressLine2 = faker.Address.SecondaryAddress(),
+        AddressLine3 = faker.Address.City(),
+        AddressLine4 = faker.Address.State(),
+        PostCode = faker.Address.ZipCode()
+      });
+    }
+
+    return result;
+  }
+
+  public static int GetAge(DateTime dob)
+  {
+    var today = DateTime.Today;
+    var age = today.Year - dob.Year;
+    if (dob.Date > today.AddYears(-age))
+      age--;
+    return age;
+  }
+}
---
+++ NitroTextFieldParser/NitroTextFieldParserTests/Models/SimpleCsvRow.cs
@@ -0,0 +1,95 @@
+﻿namespace NitroTextFieldParserTests.BenchMarks.Models;
+
+public sealed class SimpleCsvRowPopulated
+{
+  public SimpleCsvRowPopulated()
+  {
+    
+  }
+
+  public SimpleCsvRowPopulated(IList<string> fieldValues)
+  {
+    if (fieldValues.Count != 9)
+      throw new ArgumentException("CSV line contains too many fields.");
+    FirstName = fieldValues[0];
+    LastName = fieldValues[1];
+    DateOfBirth = DateTime.TryParse(fieldValues[2], out var dob) ? dob : null;
+    Age = int.TryParse(fieldValues[3], out var age) ? age : null;
+    AddressLine1 = fieldValues[4];
+    AddressLine2 = fieldValues[5];
+    AddressLine3 = fieldValues[6];
+    AddressLine4 = fieldValues[7];
+    PostCode = fieldValues[8];
+  }
+
+  public SimpleCsvRowPopulated(ReadOnlySpan<char> lineAsSpan)
+  {
+    FromCsv(lineAsSpan);
+  }
+  public string? FirstName { get; set; }
+  public string? LastName { get; set; }
+  public DateTime? DateOfBirth { get; set; }
+  public int? Age { get; set; }
+  public string? AddressLine1 { get; set; }
+  public string? AddressLine2 { get; set; }
+  public string? AddressLine3 { get; set; }
+  public string? AddressLine4 { get; set; }
+  public string? PostCode { get; set; }
+
+  public override string ToString()
+  {
+    return $"{FirstName},{LastName},{DateOfBirth?.ToString("yyyy-MM-dd")},{Age},{AddressLine1},{AddressLine2},{AddressLine3},{AddressLine4},{PostCode}";
+  }
+
+  public void FromCsv(ReadOnlySpan<char> csvLine)
+  {
+    ;
+    var fields = csvLine.Split(',');
+    var fieldIndex = 0;
+    foreach (var field in fields)
+    {
+      switch (fieldIndex)
+      {
+        case 0:
+          FirstName = field.ToString();
+          break;
+        case 1:
+          LastName = field.ToString();
+          break;
+        case 2:
+          DateOfBirth = DateTime.TryParse(field.ToString(), out var dob) ? dob : null;
+          break;
+        case 3:
+          Age = int.TryParse(field.ToString(), out var age) ? age : null;
+          break;
+        case 4:
+          AddressLine1 = field.ToString();
+          break;
+        case 5:
+          AddressLine2 = field.ToString();
+          break;
+        case 6:
+          AddressLine3 = field.ToString();
+          break;
+        case 7:
+          AddressLine4 = field.ToString();
+          break;
+        case 8:
+          PostCode = field.ToString();
+          break;
+        case 9:
+          throw new ArgumentException("CSV line contains too many fields.");
+      }
+
+      fieldIndex++;
+    }
+    if(fieldIndex<8)
+      throw new ArgumentException("CSV line contains too few fields.");
+  }
+
+  public static string GetCsvHeader()
+  {
+    return "FirstName,LastName,DateOfBirth,Age,AddressLine1,AddressLine2,AddressLine3,AddressLine4,PostCode";
+  }
+}
+
